e1500e1d9384a24ef10699cb9542d91ef37c2869
Nurudeeen <nurudeenrabiu81@gmail.com>
2023-03-24 14:45:50 +0100

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,13 @@
 function ListGroup() {
-  const cities = ["Lagos", "Abuja", "Calabar", "Port Harcout", "Kano"]
+  let cities = ['Lagos', 'Abuja', 'Calabar', 'Port Harcout', 'Kano'];
+  cities = [];
   return (
     <>
       <h1>Popular cities in 9ja</h1>
+      {cities.length===0 && <p>No item found</p>}
       <ul className="list-group">
-        {cities.map(city=> (
-          <li key = {city}>{city}</li>
+        {cities.map((city) => (
+          <li key={city}>{city}</li>
         ))}
       </ul>
     </>
